033f06a8e74343b0578b9344d546164cb1b6064e
Mateo Stjepanovic <mateostjepanovic@gmail.com>
2020-04-19 19:20:57 +0200

    Added timer

--- Assets/PlayScreenManager.cs
+++ Assets/PlayScreenManager.cs
@@ -1,25 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayScreenManager : MonoBehaviour
 {
 
-    private void Start()
+
+    public Text timerText;
+    private float timer;
+    public void Init()
     {
         if(EventManager.playScreenEnabled == null)
         {
             EventManager.playScreenEnabled = new PlayScreenEvent();
         }
+        
     }
 
+    public void Start()
+    {
+        timer = 0f;
+    }
+
+    public void Update()
+    {
+
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        timer += Time.deltaTime;
+
+        var ss = System.Convert.ToInt32(timer % 60).ToString("00");
+        var mm = (System.Math.Floor(timer / 60)).ToString("00");
+        timerText.text = mm + ":" + ss;
+
+    }
     private void OnEnable()
     {
+        if (EventManager.playScreenEnabled == null)
+        {
+            EventManager.playScreenEnabled = new PlayScreenEvent();
+        }
         EventManager.playScreenEnabled.Invoke(true);
     }
 
+
     private void OnDisable()
     {
+        if (EventManager.playScreenEnabled == null)
+        {
+            EventManager.playScreenEnabled = new PlayScreenEvent();
+        }
         EventManager.playScreenEnabled.Invoke(false);
     }
 }
